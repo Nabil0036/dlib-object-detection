17a37b61489137f8afc7653d02d6f2e46038fcae
Md. Iftekher Hossain <amiftekher@gmail.com>
2020-07-23 15:20:17 +0600

    organize code

--- test.py
+++ test.py
@@ -2,16 +2,13 @@ import pickle
 import dlib
 import cv2
 
-img = cv2.imread("D:\\hog_object_detect\\ku.jpg")
-
-detector  = dlib.simple_object_detector("det.svm")
-
+test_path = "D:\\hog_object_detect\\ku.jpg"
+img = cv2.imread(test_path)
+detector  = dlib.simple_object_detector("detector.svm")
 boxes = detector(img)
 box= boxes[0]
 (x,y,xb,yb) = [box.left(),box.top(),box.right(),box.bottom()]
-    
 cv_image = cv2.imread("D:\\hog_object_detect\\ku.jpg")
-
 cv_image = cv2.rectangle(cv_image, (x,y), (xb,yb), (255,0,0), 2)
 cv2.imshow("hola_image",cv_image)
 cv2.waitKey(0)
--- train.py
+++ train.py
@@ -1,36 +1,13 @@
 import pickle 
 import cv2
 import dlib
+from main import Train
+
 f = open('pos.pickle','rb')
 data = pickle.load(f)
 f.close()
-img = dlib.load_rgb_image("D:\\hog_object_detect\\ku.jpg")
-images =[]
-annots =[]
-for d in data:
-    p_a = d[1:]
-    annots.append([dlib.rectangle(left=p_a[0],top=p_a[1],right=p_a[2],bottom=p_a[3])])
-    t_img = d[0]
-    t_img = cv2.cvtColor(t_img, cv2.COLOR_BGR2RGB)
-    images.append(t_img)
 
-
-options = dlib.simple_object_detector_training_options()
-options.add_left_right_image_flips = True
-
-options.C = 5
-options.num_threads = 4
-options.be_verbose = True
-
-detector = dlib.train_simple_object_detector(images, annots, options)
-detector.save("det.svm")
-boxes = detector(img)
-box= boxes[0]
-(x,y,xb,yb) = [box.left(),box.top(),box.right(),box.bottom()]
-    
-cv_image = cv2.imread("D:\\hog_object_detect\\ku.jpg")
-
-cv_image = cv2.rectangle(cv_image, (x,y), (xb,yb), (255,0,0), 2)
-cv2.imshow("hola_image",cv_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+train = Train(data)
+images,annotations = train._prepare_image_and_annotations()
+detector = train.trainer(images,annotations)
+detector.save("detector.svm")
